f7dfc1dc497d31072f31e6bf1daf260e225a1de9
Romain Mormont <romainmormont@hotmail.com>
2018-07-18 15:35:51 +0200

    refactor mask_to_objects tests

--- tests/test_mask_to_objects.py
+++ tests/test_mask_to_objects.py
@@ -1,53 +1,11 @@
-from unittest import TestCase, skip
+from unittest import TestCase
 
 import numpy as np
-from PIL.Image import fromarray
-from PIL.ImageDraw import ImageDraw
-from cv2 import imwrite
 
 from annotation_exporter import mask_to_objects_2d
-from shapely.geometry import Point, Polygon, box
+from shapely.geometry import Polygon, box
 
-
-def draw_square(image, side, center, color):
-    """Draw a square centered in 'center' and of which the side has 'side'"""
-    top_left = (center[1] - side / 2, center[0] - side / 2)
-    top_right = (center[1] + side / 2, center[0] - side / 2)
-    bottom_left = (center[1] - side / 2, center[0] + side / 2)
-    bottom_right = (center[1] + side / 2, center[0] + side / 2)
-    p = Polygon([top_left, top_right, bottom_right, bottom_left, top_left])
-    return draw_poly(image, p, color)
-
-
-def draw_square_by_corner(image, side, top_left, color):
-    top_left = (top_left[1], top_left[0])
-    top_right = (top_left[0] + side, top_left[1])
-    bottom_left = (top_left[0], top_left[1] + side)
-    bottom_right = (top_left[0] + side, top_left[1] + side)
-    p = Polygon([top_left, top_right, bottom_right, bottom_left, top_left])
-    return draw_poly(image, p, color)
-
-
-def draw_circle(image, radius, center, color=255, return_circle=False):
-    """Draw a circle of radius 'radius' and centered in 'centered'"""
-    circle_center = Point(*center)
-    circle_polygon = circle_center.buffer(radius)
-    image_out = draw_poly(image, circle_polygon, color)
-    if return_circle:
-        return image_out, circle_polygon
-    else:
-        return image_out
-
-
-def draw_poly(image, polygon, color=255):
-    """Draw a polygon in the given color at the given location"""
-    pil_image = fromarray(image)
-    validated_color = color
-    draw = ImageDraw(pil_image)
-    if len(image.shape) > 2 and image.shape[2] > 1:
-        validated_color = tuple(color)
-    draw.polygon(polygon.boundary.coords, fill=validated_color, outline=validated_color)
-    return np.asarray(pil_image)
+from tests.util import draw_square_by_corner, draw_poly
 
 
 class TestMaskToObject2D(TestCase):
@@ -132,7 +90,6 @@ class TestMaskToObject2D(TestCase):
         image = draw_poly(image, Polygon([(15, 77), (15, 78), (16, 78), (15, 77)]), color=127)
         image = draw_poly(image, box(1, 1, 2, 2), color=255)
 
-        imwrite("test.png", image)
         slices = mask_to_objects_2d(image)
         # sort by bounding box top left corner
         slices = sorted(slices, key=lambda s: s.polygon.bounds[:2])
